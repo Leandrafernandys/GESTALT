bd3d2ceb963ca0650970def98953b9bca8bd3075
leandrafernandys <leahfernandys@gmail.com>
2021-05-15 15:36:05 -0300

    feito

--- js/code.js
+++ js/code.js
@@ -1,1 +1,27 @@
 AOS.init();
+
+
+// seta
+$(window).scroll(function(){
+    if($(window).scrollTop() >= 550)
+    {
+        $('.seta').fadeIn();
+    }else{
+        $('.seta').fadeOut();
+    }
+});
+
+$('.seta').click(function(){
+    $('body, html').animate({
+        scrollTop: 0
+    },1200);
+});
+
+
+$('.rolagem').click(function(){
+    var id= $(this).attr('href');
+    var destino = $(id).offset().top;
+    $('html, body').animate({
+        scrollTop: destino,
+    });
+});
